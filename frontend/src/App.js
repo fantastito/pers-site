891eb4079c350a2de9e8bb2f570efa805e9ac2cb
J. Simon Budden <20130247@j-simon-buddens-macbook.local>
2024-04-16 11:58:37 +0100

    Building site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { TitleCard } from './components/TitleCard';
+import { AboutMe } from './components/AboutMe';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <TitleCard/>
+        <AboutMe/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simon's personal website.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ frontend/src/components/TitleCard.jsx
@@ -0,0 +1,11 @@
+
+
+export const TitleCard = () => {
+    return (
+        <div id="titleCard">
+            <h1>SIMON BUDDEN</h1>
+            <h2>software engineer</h2>
+        </div>
+
+    )
+}
