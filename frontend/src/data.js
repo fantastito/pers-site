b7f238ee044efa41e2503ce8126b22980c522820
J. Simon Budden <20130247@j-simon-buddens-macbook.local>
2024-04-18 12:29:15 +0100

    Data redeveloped as JSON object

---
+++ frontend/src/data.js
@@ -0,0 +1,85 @@
+export const data = {
+    aboutme: {
+        name: "Simon Budden",
+        title: "Software Developer",
+        location: "London, UK",
+        email: "simon.budden@gmail.com",
+        summary: [
+            "Junior full-stack software engineer.",
+            "Experience with Python, JavaScript, No/SQL databases, React, APIs, test-driven development, object-oriented programming.",
+            "A decade of comms and policy experience in Brussels and London, interpreting the European Union and securing ports.",
+            "Now looking to use passion for crafting solutions to build things."
+        ],
+        skills: ["JavaScript", "Python", "HTML/CSS", "SQL", "Git", "Agile methodologies", "Project management", "Stakeholder management", "Copywriting"]
+    },
+    experience: [
+        {
+            company: "Makers Academy",
+            date: "Oct. 2023 to Feb. 2024",
+            role: "Coding and Software Development Bootcamp",
+            details: [
+                "Python-based coding foundations module covering basics of coding and programmes' structure and cloud deployment.",
+                "Software development module focusing on JavaScript and web applications, including RESTful APIs and agile group projects.",
+                "Underscored importance of modern software development practices such as using Git and GitHub, pair programming, test-driven development, etc."
+            ],
+            skills: ["JavaScript", "Python", "Postgres", "Flask", "Visual Studio Code", "Jest", "Swift", "Xcode"]
+        },
+        {
+            company: "Dept. for Science, Innovation and Technology, London",
+            date: "April to Oct. 2023",
+            role: "Cyber security policy advisor",
+            details: [
+                "Developed new UK Internet of Things cyber security legislation and collaborated on implementation of enforcement regime."
+            ],
+            skills: ["Security requirements", "Compliance", "Excel"]
+        },
+        {
+            company: "Dept. for Transport",
+            date: "Sept. 2020 to April 2023",
+            role: "Maritime security policy advisor",
+            details: [
+                "Developed the UK’s legal and practical maritime security regime, working with security colleagues across government.",
+                "Established network of European counterparts and conducted a post-implementation review of legislation.",
+                "Interpreted novel legislation and designed workflow for enforcement at pace, which was used as a template by a sister division."
+            ],
+            skills: ["Excel", "Security requirements", "Indsutry engagement", "Problem solving", "Attention to detail", "Teamwork"]
+        },
+        {
+            company: "European Parliament",
+            date: "June 2015 to Sept. 2020",
+            role: "Communications officer",
+            details: [
+                "Worked on bringing together the European Parliament's visitor services, including renewing common visiting website.",
+                "Developed engaging comms materials for online, print and facilities.",
+                "Implemented and managed high-visibility projects to improve the parliament campus."
+            ],
+            skills: ["Copywriting", "Website development", "Excel", "Public speaking", "Stakeholder management", "Project management"]
+        },
+    ],
+    projects: [
+        {
+            title: "TutorMatch",
+            description: "A web app that connects volunteer tutors with students in need of some extra help. Tutors can indicate their subjects and availability; students can search for tutors and request a slot. Try logging in as stu@email.com / Password123!",
+            link: "https://github.com/KatBiel/TutorMatch",
+            tech: ["Flask", "React", "MongoDB", "Python", "Node.js", "bootstrap", "firebase", "Postman", "Trello", "Visual Studio"]
+        },
+        {
+            title: "Acebook",
+            description: "A social media web app developed from legacy code.",
+            link: "https://github.com/fantastito/Acebook",
+            tech: ["MongoDB", "Express", "React", "Node.js", "CSS", "Postman", "Trello", "Visual Studio"]
+        },
+        {
+            title: "Personal website",
+            description: "A portfolio website and platform for projects.",
+            link: "https://github.com/fantastito/pers-site",
+            tech: ["MongoDB", "Express", "React", "Node.js", "Tailwind", "Visual Studio"]
+        },
+        {
+            title: "Acebook Mobile",
+            description: "An iOS frontend for a MERN social media app develped from a seed repo.",
+            link: "https://github.com/fantastito/Acebook-Mobile",
+            tech: ["SwiftUI", "XCode", "Cloudinary", "Postman", "Trello"]
+        }
+    ]
+};
