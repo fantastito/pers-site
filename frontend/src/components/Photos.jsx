71bca34509a4d09dbc90f80fb47509f3f8501336
J. Simon Budden <20130247@j-simon-buddens-macbook.local>
2024-05-23 20:42:29 +0100

    Photos title padding changed

--- frontend/src/components/Photos.jsx
+++ frontend/src/components/Photos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Photos() {
   return (
     <div>
-      <div id="title-card-content" className="bg-white text-black pt-8 sm:pt-8 md:pt-8 lg:pt-8 px-5 pb-8">
+      <div id="photos-content" className="bg-white text-black pt-4 sm:pt-4 md:pt-4 lg:pt-4 px-5 pb-8">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">PHOTOGRAPHY</h1>
       </div>
       <div className="p-4">
