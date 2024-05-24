6cb00a11d3533c224dc2ca21440bac8fb96ba1e6
J. Simon Budden <20130247@j-simon-buddens-macbook.local>
2024-05-24 11:41:13 +0100

    Photo gallery added

--- frontend/src/components/Photos.jsx
+++ frontend/src/components/Photos.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import Gallery from 'react-photo-gallery'
+
+const photos = [
+  {
+    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 1,
+    height: 1,
+  },
+  // Add more photo objects here
+];
 
 function Photos() {
   return (
@@ -6,10 +21,7 @@ function Photos() {
       <div id="photos-content" className="bg-white text-black pt-4 sm:pt-4 md:pt-4 lg:pt-4 px-5 pb-8">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">PHOTOGRAPHY</h1>
       </div>
-      <div className="p-4">
-        <p className="mb-4">Photos go here.</p>
-      </div>
-
+      <Gallery photos={photos} />
     </div>
   );
 }
