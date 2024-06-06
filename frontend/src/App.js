339609db2275eecfe94130c7f6edc50875eb2af0
Simon Budden <simon.budden@gmail.com>
2024-06-06 11:31:34 +0100

    Tidy up App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Resume from './components/Resume';
 import Photos from './components/Photos';
-import Quiz from './components/Quiz'
+// import Quiz from './components/Quiz'
 import Footer from './components/Footer';
 
 function App() {
