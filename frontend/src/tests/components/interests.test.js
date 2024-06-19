c5365d2251fbc4552d81cece231b55a60906068b
Simon Budden <simon.budden@gmail.com>
2024-06-19 14:33:53 +0100

    Update to interests test

--- frontend/src/tests/components/interests.test.js
+++ frontend/src/tests/components/interests.test.js
@@ -3,10 +3,12 @@ import { Interests } from '../../components/Interests';
 
 describe('generate Interests component', () => {
     it('should generate the Interests section correction', () => {
-        const mockInterets = ["F1", "Photography","Hiking", "Campari spritz"];
+        const mockInterests = ["F1", "Photography", "Hiking", "Campari spritz"];
 
-        render(<Interests interestsData={mockInterets} />);
+        render(<Interests interestsData={mockInterests} />);
 
-        expect(screen.getByText("F1")).toBeInTheDocument();
+        mockInterests.forEach(interest => {
+            expect(screen.getByText(interest)).toBeInTheDocument();
+        });
     });
 });
